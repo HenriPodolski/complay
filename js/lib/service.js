96790d5158537b95eeb190eb74cc3cf27e6caf35
HenriPodolski <henrip@gmail.com>
2016-04-15 15:09:18 +0200

    implements basic ajax fetch method

--- js/lib/service.js
+++ js/lib/service.js
@@ -3,7 +3,7 @@
  * used to create models, collections, proxies, adapters
  */
 import Base from './base';
-import DefaultReducers as ServiceReducers from '../extensions/service/reducers/default-reducers';
+import DefaultReducers from '../extensions/services/reducers/default-reducers';
 import assign from '../helpers/object/assign';
 import isArrayLike from '../helpers/array/is-array-like';
 import merge from '../helpers/array/merge';
@@ -37,10 +37,10 @@ class Service extends Base {
 
 		this.data = {};
 
-		// composing this with ServiceReducers via this.data
-		for (var method in ServiceReducers) {	
-			if (ServiceReducers.hasOwnProperty(method)) {
-				this.data[method] = ServiceReducers[method].bind(this);
+		// composing this with DefaultReducers via this.data
+		for (var method in DefaultReducers) {	
+			if (DefaultReducers.hasOwnProperty(method)) {
+				this.data[method] = DefaultReducers[method].bind(this);
 			}
 		}		
 
--- test/spec/unit/extensions/service/conduitjs-service-extension-ajax.spec.js
+++ test/spec/unit/extensions/service/conduitjs-service-extension-ajax.spec.js
@@ -1,6 +1,5 @@
 import ajaxExtension from '../../../../../js/extensions/services/remote/ajax';
 import Service from '../../../../../js/lib/service';
-import getGlobalObject from '../../../../../js/helpers/environment/get-global-object';
 import mix from '../../../../../js/helpers/object/mix';
 import chai from 'chai';
 import sinon from 'sinon';
@@ -9,38 +8,74 @@ var expect = chai.expect;
 var asset = chai.assert;
 chai.should();
 
+let glob = {};
+
 describe('Conduitjs JS Ajax Extension', ()=>{
 
-	let xhr;
-	let requests;
-	let ajax;
-	let originalXMLHttpRequest;
-	let glob = getGlobalObject();
+	describe('base', () => {
+		beforeEach(function() {
+			
+			glob.xhr = sinon.useFakeXMLHttpRequest();
+			var requests = glob.requests = [];
 
-	beforeEach(() => {
-		xhr = sinon.useFakeXMLHttpRequest();
-		originalXMLHttpRequest = glob.XMLHttpRequest;
-		glob.XMLHttpRequest = xhr;
-		
-		requests = [];
-		
-		/* eslint no-shadow: 1*/
-		xhr.onCreate = (xhr) => {
-			requests.push(xhr);
-		};
+			glob.xhr.onCreate = function (xhr) {
+				requests.push(xhr);
+			};
 
-		// ajaxExtension.resource = 'https://example.com/test';
+			ajaxExtension.resource = {url: 'https://example.com/test'}
+		});
+
+		afterEach(function() {
+			glob.xhr.restore();
+		});
+
+	    it('should have getters and setters for resource', () => {
+	    	expect(ajaxExtension).to.have.property('resource');
+	    });
+
+		it('should construct with resource property given through parameters', () => {
+			expect(ajaxExtension.resource.url).to.equal('https://example.com/test');
+		});
 	});
 
-	afterEach(() => {
-      xhr.restore();
-    });
+	describe('fetch/save', () => {
+		beforeEach(function() {
+			
+			glob.xhr = sinon.useFakeXMLHttpRequest();
+			var requests = glob.requests = [];
 
-    after(() => {
-    	glob.XMLHttpRequest = originalXMLHttpRequest;
-    });
+			glob.xhr.onCreate = function (xhr) {
+				requests.push(xhr);
+			};
 
-	it('should construct with resource property given through parameters', () => {
-		expect(ajaxExtension.resource).to.equal('https://example.com/test');
+			ajaxExtension.resource = {url: 'https://example.com/test', method: 'GET'}
+		});
+
+		afterEach(function() {
+			glob.xhr.restore();
+		});
+
+	    it('should fetch data from the server', (done) => {
+			
+			ajaxExtension.fetch()
+				.then((res) => { 
+					expect(JSON.parse(res)).to.deep.equal({"id": 1, "name": "foo"}); 
+					done();
+				})
+				.catch((err) => { console.log(err); });
+			
+			expect(glob.requests.length).to.equal(1);
+			
+			glob.requests[0].respond(200,
+				{"Content-Type": "application/json"},
+				'{"id": 1, "name": "foo"}'
+			);
+
+			// sinon.assert.calledWith(callback, {"id": 1, "name": "foo"});
+		});
+
+		it('should send data to the server', () => {
+			expect(false).to.equal('Todo: Implement this!');
+		});
 	});
 });
