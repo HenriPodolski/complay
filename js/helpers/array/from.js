98fe4d9d0075a288272a1e3b3627836d9265e037
HenriPodolski <henrip@gmail.com>
2016-04-15 15:26:43 +0200

    removed

--- js/helpers/array/from.js
+++
@@ -1,8 +0,0 @@
-export default (function() {
-	if (!Array.from) {
-		Array.from = function (object) {
-			'use strict';
-			return [].slice.call(object);
-		};
-	}
-}.call(this))
--- js/helpers/string/dasherize.js
+++
@@ -1,5 +0,0 @@
-export default function dasherize(str) {
-	return str.replace(/[A-Z]/g, function(char, index) {
-		return (index !== 0 ? '-' : '') + char.toLowerCase();
-	});
-};
